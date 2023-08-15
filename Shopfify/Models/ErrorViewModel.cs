fc5a7e2651166fc89f8da6ae51bcd9f71fbabe38
jayca <jayca@JULIUS-LAPTOP>
2023-08-15 23:47:44 +0800

    Add project files.

---
+++ Shopfify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopfify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
